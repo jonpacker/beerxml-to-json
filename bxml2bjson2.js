5966c59671424f7054789cc7eb5e64f59794a2fd
Jon Packer <contact@jonpacker.com>
2013-06-08 16:41:47 +0200

    Reading hops

--- bxml2bjson2.js
+++ bxml2bjson2.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var parseXml = require('xml2js').parseString;
-var path = require('JSONPath').eval;
+var jsonPath = require('JSONPath').eval;
 var async = require('async');
 
 fs.readFile(process.argv[2], 'utf8', function(err, inputXml) {
@@ -11,7 +11,17 @@ fs.readFile(process.argv[2], 'utf8', function(err, inputXml) {
   });
 });
 
-
+var floatify = function(resolver, modifier) {
+  return function(callback) {
+    resolver(function(err, val) {
+      if (err) return callback(err);
+      val = parseFloat(val);
+      if (isNaN(val)) val = undefined;
+      else if (typeof modifier == 'function') val = modifier(val);
+      callback(null, val);
+    })
+  };
+};
 
 function createBeerJson(beerxml, cb) {
   if (!beerxml.RECIPES) {
@@ -30,13 +40,36 @@ function createBeerJson(beerxml, cb) {
 
   var recipe = beerxml.RECIPES.RECIPE[0];
 
-  var atPath = function(jsonPath) { return path(beerxml, jsonPath); }
-  var asyncPath = function(path) {
-    return function(callback) { callback(null, atPath(path)) };
+  var valAt = function(path) {
+    return function(callback) { callback(null, jsonPath(recipe, path)[0]) };
   };
+  var floatAt = function(path, mod) { return floatify(valAt(path), mod) }
+
+  var percent = function(n) { return n / 100 };
 
   var prototype = {
-    name: asyncPath('$.NAME[0]')
+    name: valAt('$.NAME[0]'),
+    style: valAt('$.STYLE[0].NAME[0]'),
+    brewer: valAt('$.BREWER[0]'),
+    batchSize: floatAt('$.BATCH_SIZE[0]'),
+    boilSize: floatAt('$.BOIL_SIZE[0]'),
+    boilTime: floatAt('$.BOIL_TIME[0]'),
+    efficiency: floatAt('$.EFFICIENCY[0]', percent),
+    og: floatAt('$.OG[0]'),
+    fg: floatAt('$.FG[0]'),
+    hops: function(cb) {
+      var hops = jsonPath(recipe, '$.HOPS[0].HOP.*').map(function(hopData) {
+        var hop = {};
+        if (hopData.NAME) hop.name = hopData.NAME[0];
+        if (hopData.ALPHA) hop.aa = parseFloat(hopData.ALPHA[0]);
+        if (hopData.AMOUNT) hop.amount = parseFloat(hopData.AMOUNT[0]);
+        if (hopData.USE) hop.use = hopData.USE[0];
+        if (hopData.TIME) hop.time = parseFloat(hopData.TIME);
+        if (hopData.FORM) hop.form = hopData.FORM[0];
+        return hop;
+      });
+      cb(null, hops);
+    }
   };
 
   async.auto(prototype, function(err, result) {
